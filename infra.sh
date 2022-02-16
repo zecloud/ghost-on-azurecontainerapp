70e5bc522f4fc63e866ece5b451c732fbca7284f
Aymeric Weinbach <397730+zecloud@users.noreply.github.com>
2022-02-16 09:21:29 +0000

    infra script

---
+++ infra.sh
@@ -0,0 +1,41 @@
+#az extension add \
+#  --source https://workerappscliextension.blob.core.windows.net/azure-cli-extension/containerapp-0.2.2-py2.py3-none-any.whl
+#az provider register --namespace Microsoft.Web
+RESOURCE_GROUP="ghost-container-app"
+LOCATION="westeurope"
+LOG_ANALYTICS_WORKSPACE="ghost-container-app-logs"
+CONTAINERAPPS_ENVIRONMENT="ghost-container-app-env"
+MYSQL_SERVER_NAME="ghost-dbserver"
+az group create \
+  --name $RESOURCE_GROUP \
+  --location $LOCATION
+az monitor log-analytics workspace create \
+  --resource-group $RESOURCE_GROUP \
+  --workspace-name $LOG_ANALYTICS_WORKSPACE
+LOG_ANALYTICS_WORKSPACE_CLIENT_ID=`az monitor log-analytics workspace show --query customerId -g $RESOURCE_GROUP -n $LOG_ANALYTICS_WORKSPACE -o tsv | tr -d '[:space:]'`
+LOG_ANALYTICS_WORKSPACE_CLIENT_SECRET=`az monitor log-analytics workspace get-shared-keys --query primarySharedKey -g $RESOURCE_GROUP -n $LOG_ANALYTICS_WORKSPACE -o tsv | tr -d '[:space:]'`
+az containerapp env create \
+  --name $CONTAINERAPPS_ENVIRONMENT \
+  --resource-group $RESOURCE_GROUP \
+  --logs-workspace-id $LOG_ANALYTICS_WORKSPACE_CLIENT_ID \
+  --logs-workspace-key $LOG_ANALYTICS_WORKSPACE_CLIENT_SECRET \
+  --location $LOCATION
+
+CONTAINER_APP_ENV_IP=`az containerapp env show -g $RESOURCE_GROUP -n $CONTAINERAPPS_ENVIRONMENT --query staticIp -o tsv`
+az mysql flexible-server create --location $LOCATION --resource-group  $RESOURCE_GROUP \
+  --name $MYSQL_SERVER_NAME --admin-user $DB_USER --admin-password $DB_PASSWORD \
+  --sku-name Standard_B1s  --public-access $CONTAINER_APP_ENV_IP  --storage-auto-grow Enabled 
+
+  #--iops 320 --tier Burstable --storage-size 32
+
+az containerapp create \
+  --name ghost-in-zecloud \
+  --resource-group $RESOURCE_GROUP \
+  --environment $CONTAINERAPPS_ENVIRONMENT \
+  --image ghost:latest \
+  --target-port 2368 \
+  --ingress 'external' \
+  --query configuration.ingress.fqdn \
+  --environment-variables "database__client=mysql,database__connection__host=$MYSQL_SERVER_NAME.mysql.database.azure.com \
+      ,database__connection__user=$DB_USER,database__connection__password=$DB_PASSWORD \
+      ,database__connection__database=ghost,database__connection__ssl=true,database__connection__port=3306"
